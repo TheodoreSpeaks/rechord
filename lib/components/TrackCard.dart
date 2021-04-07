9dc2e1c73e787e25829c54d7ed4e82c95af8fa5e
Theodore Li <theodoreqili@gmail.com>
2021-04-06 23:47:12 -0500

    Remove starter code, create components folder

---
+++ lib/components/TrackCard.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class TrackCard extends StatelessWidget {
+  const TrackCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.red, Colors.purple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(32)),
+        padding: EdgeInsets.all(16.0),
+        margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+        height: 250,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(children: [
+              Spacer(),
+              Icon(
+                Icons.music_note,
+                color: Colors.white,
+              ),
+              Text(
+                'Rock',
+                style: TextStyle(color: Colors.white),
+              )
+            ]),
+            Text(
+              'Created a funky bassline, let me know what you think!',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            Text(
+              '@TheodoreSpeaks',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            Spacer(),
+            Row(
+              children: [
+                FloatingActionButton(
+                  backgroundColor: Colors.lightGreen,
+                  child: Icon(
+                    Icons.play_arrow,
+                    size: 36,
+                  ),
+                ),
+                Spacer(),
+                buildTrackVisualization(),
+                Spacer(),
+                Column(
+                  children: [
+                    Text(
+                      '36',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.white,
+                      size: 36,
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
+  }
+
+  Widget buildTrackVisualization() {
+    List<Widget> lines = [];
+    for (int i = 0; i < 35; i++) {
+      lines.add(Container(
+        margin: EdgeInsets.all(1),
+        decoration: BoxDecoration(
+            color: Colors.white70, borderRadius: BorderRadius.circular(30)),
+        width: 6,
+        height: 24 * (sin(i * pi / 4) + 1),
+      ));
+    }
+    return Row(
+      children: lines,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+
+import 'components/TrackCard.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,22 +28,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,19 +43,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,99 +63,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: Text('Create Track'),
+        icon: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class TrackCard extends StatelessWidget {
-  const TrackCard({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.red, Colors.purple],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight),
-            borderRadius: BorderRadius.circular(32)),
-        padding: EdgeInsets.all(16.0),
-        margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
-        height: 250,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(children: [
-              Spacer(),
-              Icon(
-                Icons.music_note,
-                color: Colors.white,
-              ),
-              Text(
-                'Rock',
-                style: TextStyle(color: Colors.white),
-              )
-            ]),
-            Text(
-              'Created a funky bassline, let me know what you think!',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            Text(
-              '@TheodoreSpeaks',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            Spacer(),
-            Row(
-              children: [
-                FloatingActionButton(
-                  backgroundColor: Colors.lightGreen,
-                  child: Icon(
-                    Icons.play_arrow,
-                    size: 36,
-                  ),
-                ),
-                Spacer(),
-                buildTrackVisualization(),
-                Spacer(),
-                Column(
-                  children: [
-                    Text(
-                      '36',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    Icon(
-                      Icons.favorite_border,
-                      color: Colors.white,
-                      size: 36,
-                    )
-                  ],
-                )
-              ],
-            )
-          ],
-        ));
-  }
-
-  Widget buildTrackVisualization() {
-    List<Widget> lines = [];
-    for (int i = 0; i < 35; i++) {
-      lines.add(Container(
-        margin: EdgeInsets.all(1),
-        decoration: BoxDecoration(
-            color: Colors.white70, borderRadius: BorderRadius.circular(30)),
-        width: 6,
-        height: 24 * (sin(i * pi / 4) + 1),
-      ));
-    }
-    return Row(
-      children: lines,
     );
   }
 }
