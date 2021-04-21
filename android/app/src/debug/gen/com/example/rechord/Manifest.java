f99e1995ed3b6eb640af88cd81af4412bc52d9d1
SpicyOwl <duanxu@bc.edu>
2021-04-21 01:48:47 -0400

    init

---
+++ android/app/src/debug/gen/com/example/rechord/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.rechord;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
